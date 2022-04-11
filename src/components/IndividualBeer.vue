c18d48db9ec39b52f01293ba4b507ccee0984a49
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-04-11 21:39:33 +0100

    Pass props to component and render it

--- src/components/IndividualBeer.vue
+++ src/components/IndividualBeer.vue
@@ -1,10 +1,14 @@
 <template>
   <div>{{ id }}</div>
+  <p>{{name}}</p>
+  <img :src="image" :alt="`${name} image`" />
+  <p>{{description}}</p>
+  <p>{{tagline}}</p>
 </template>
 
 <script>
 export default {
   name: 'IndividualBeer',
-  props: ['id']
+  props: ['id', 'name', 'image', 'tagline', 'description']
 }
 </script>
