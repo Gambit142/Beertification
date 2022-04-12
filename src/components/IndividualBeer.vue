f1f0ad71ea309c2766b55cf3ec1ec8cef60c31a0
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-04-12 21:44:42 +0100

    Style the IndividualBeer component

--- src/components/IndividualBeer.vue
+++ src/components/IndividualBeer.vue
@@ -1,14 +1,68 @@
 <template>
-  <div>{{ id }}</div>
-  <p>{{name}}</p>
-  <img :src="image" :alt="`${name} image`" />
-  <p>{{description}}</p>
-  <p>{{tagline}}</p>
+  <v-hover
+    v-slot="{ isHovering, props }"
+  >
+    <v-card :elevation="isHovering ? 14 : 2" v-bind="props" class="rounded-shaped">
+      <div class="my-6">
+        <v-img :src="image" :alt="`${name} image`" class="bg-white" height="400">
+          <template v-slot:placeholder>
+            <v-row
+              class="fill-height ma-0"
+              align="center"
+              justify="center"
+            >
+              <v-progress-circular
+                indeterminate
+                color="orange-darken-3"
+              ></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+      </div>
+      <div class="d-flex justify-sm-center align-sm-start mb-3 name-height">
+        <v-card-title class="text-center text-h6 font-weight-black">{{name}}</v-card-title>
+      </div>
+      <div class="d-flex justify-sm-center align-sm-start tagline-height">
+        <v-card-subtitle class="text-orange-darken-4 text-h6 text-center">{{tagline}}</v-card-subtitle>
+      </div>
+      <router-link :to="`/beer/${id}`" class="text-decoration-none">
+        <div class="d-flex justify-center">
+          <v-btn
+          variant="outlined"
+          rounded="lg"
+          class="bg-orange-accent-1 font-weight-black btn"
+          block
+          >See Details
+          </v-btn>
+        </div>
+      </router-link>
+    </v-card>
+  </v-hover>
 </template>
 
 <script>
 export default {
   name: 'IndividualBeer',
-  props: ['id', 'name', 'image', 'tagline', 'description']
+  props: ['id', 'name', 'image', 'tagline']
 }
 </script>
+<style scoped>
+  .rounded-shaped {
+    border-width: 2px;
+  }
+
+  .btn:hover {
+    background-color: #fff !important;
+    color: #e65100 !important;
+  }
+
+  @media screen and (min-width: 600px) {
+    .name-height {
+      height: 5rem;
+    }
+
+    .tagline-height {
+      height: 6rem;
+    }
+  }
+</style>
