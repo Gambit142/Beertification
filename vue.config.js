49d906ab0b3a8c1249932ed39330b739a7e2be7d
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-04-14 09:48:23 +0100

    Correct the syntax for the publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 
-module.exports = {
-  publicPath: '/Beertification/'
-}
-
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: process.env.NODE_ENV === 'production' ? '/Beertification/' : '/',
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
